a6eb66dfc4bcb18454733dfe1923426be3b2dae7
lhj657666 <74294982+lhj657666@users.noreply.github.com>
2020-11-25 17:44:04 +0800

    Create 112505.小霸王猜数字.app.c
    今天第五次上交作业

---
+++ 112505.小霸王猜数字.app.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <windows.h>
+void main()
+{
+	while(1){
+		int a=5;
+		int b;
+ printf("猜数字大赛，如果你猜中了会有奖励噢\n");
+ printf("友情提示:请猜0-10的数字\n");
+ scanf("%d",&b);
+ if (b==a)
+ {
+ printf("恭喜小猪，你猜对了，你的奖励是来自帅气(美丽)的老师的悉心教导\n");
+ }
+ else if (b>6)
+ {
+ printf("不好意思，小猪你猜大了，你还可以继续猜数字噢\n");
+ }
+ else
+ {
+ printf("不好意思，小猪你猜小了，你还可以继续猜数字噢\n");
+ }
+	}
+}
