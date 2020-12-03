9f0711fdc30193f0a9dd2e1dc3e88ea2a205049c
lhj657666 <74294982+lhj657666@users.noreply.github.com>
2020-12-03 21:49:05 +0800

    Create 120303.数组清零.app.c
    作业3

---
+++ 120303.数组清零.app.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <windows.h>
+void main ()
+{
+	int a,b;
+int array[]={2020,2021,2022,2023};
+int array2d[20][10]={
+	{1,2,3},
+	{4,5,6}
+};
+for (a=0;a<20;a++){
+	for (b=0;b<10;b++){
+    array2d[a][b]=999;
+	printf("%d ",array2d[a][b]);
+	}
+	printf("\n");
+	}
+    Sleep(5000);
+for (a=0;a<20;a++){
+	for (b=0;b<10;b++){
+    array2d[a][b]=999;
+	printf("%d ",array2d[a][b]);
+	}
+	printf("\n");
+}
+}
