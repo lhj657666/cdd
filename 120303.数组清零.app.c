b01ed4daae0e85b51d5357cce2ca2e7b8e3706ae
lhj657666 <74294982+lhj657666@users.noreply.github.com>
2020-12-09 08:36:37 +0800

    Update 120303.数组清零.app.c
    修改上一次的作业

--- 120303.数组清零.app.c
+++ 120303.数组清零.app.c
@@ -15,8 +15,14 @@ for (a=0;a<20;a++){
 	}
 	printf("\n");
 	}
+for (a=0;a<20;a++){
+	for (b=0;b<10;b++){
+    array2d[a][b]=0;
+	printf("%d ",array2d[a][b]);
+	}
+	printf("\n");
+}
     Sleep(5000);
-printf("不好意思，我真的不会归零。求:不要挂科。老师最帅，笔芯*\n");
 for (a=0;a<20;a++){
 	for (b=0;b<10;b++){
     array2d[a][b]=999;
